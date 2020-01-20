050f7f136409ee81baeaa062c32d5d7cd5d013bb
Przemek Gawlowski <przemek.gawlowski@gamelounge.com>
2020-01-20 12:37:08 +0100

    Use Composition API

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueCompositionApi from '@vue/composition-api'
 import './plugins/fontawesome'
 import App from './App.vue'
 import router from './router'
@@ -6,6 +7,7 @@ import store from './store'
 import AppButton from './components/AppButton'
 
 Vue.component('AppButton', AppButton)
+Vue.use(VueCompositionApi)
 
 Vue.config.productionTip = false
 
