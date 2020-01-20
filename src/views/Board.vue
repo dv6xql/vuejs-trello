7cfbd79b20f266c45c8e3671dffe02817630d632
Przemek Gawlowski <przemek.gawlowski@gamelounge.com>
2020-01-20 10:41:20 +0100

    Use BoardColumn component

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,40 +1,13 @@
 <template>
     <div class="board">
+        <board-column
+                v-for="(column, $columnIndex) of board.columns"
+                :key="$columnIndex"
+                :column="column"
+                :columnIndex="$columnIndex"
+        ></board-column>
+
         <div class="flex flex-row items-start">
-            <div class="column"
-                 v-for="(column, $columnIndex) of board.columns"
-                 :key="$columnIndex"
-                 draggable
-                 @drop="moveTaskOrColumn($event, column.tasks, $columnIndex)"
-                 @dragover.prevent
-                 @dragenter.prevent
-                 @dragstart.self="pickupColumn($event, $columnIndex)">
-                <div class="flex items-center mb-2 font-bold">
-                    {{ column.name }}
-                </div>
-                <div class="list-reset">
-                    <div class="task"
-                         v-for="(task, $taskIndex) of column.tasks"
-                         :key="$taskIndex"
-                         @click="goToTask(task)"
-                         draggable
-                         @dragstart="pickupTask($event, $taskIndex, $columnIndex)"
-                         @dragover.prevent
-                         @dragenter.prevent
-                         @drop.stop="moveTaskOrColumn($event, column.tasks, $columnIndex, $taskIndex)">
-                        <span class="w-full flex-no-shrink font-bold">
-                            {{ task.name }}
-                        </span>
-                        <p class="w-full flex-no-shrink mt-1 text-sm" v-if="task.description">
-                            {{ task.description }}
-                        </p>
-                    </div>
-
-                    <input type="text" class="block p-2 w-full bg-transparent" placeholder="+ Enter new task"
-                           @keyup.enter="createTask($event, column.tasks)">
-                </div>
-            </div>
-
             <div class="column flex">
                 <input type="text" class="p-2 mr-2 flex-grow" placeholder="New Column Name" v-model="newColumnName"
                        @keyup.enter="createColumn()">
@@ -49,8 +22,12 @@
 
 <script>
     import {mapState} from 'vuex'
+    import BoardColumn from "../components/BoardColumn"
 
     export default {
+        components: {
+            BoardColumn
+        },
         data() {
             return {
                 newColumnName: ''
@@ -133,11 +110,6 @@
 <style lang="css">
     .task {
         @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
-    }
-
-    .column {
-        @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
-        min-width: 350px;
     }
 
     .board {
