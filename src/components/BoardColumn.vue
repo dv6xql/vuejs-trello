d82656460392c1157f8c63c05257f60a6beeb76c
Przemek Gawlowski <przemek.gawlowski@gamelounge.com>
2020-01-20 11:42:41 +0100

    Use AppDrop component

--- src/components/BoardColumn.vue
+++ src/components/BoardColumn.vue
@@ -1,34 +1,37 @@
 <template>
-    <app-drag :transferData="getTransferData(columnIndex)">
-        <div class="column"
-             @drop="moveTaskOrColumn($event, column.tasks, columnIndex)"
-             @dragover.prevent
-             @dragenter.prevent>
-            <div class="flex items-center mb-2 font-bold">
-                {{ column.name }}
-            </div>
-            <div class="list-reset">
-                <ColumnTask
-                        v-for="(task, $taskIndex) of column.tasks"
-                        :key="$taskIndex"
-                        :task="task"
-                        :taskIndex="$taskIndex"
-                        :column="column"
-                        :columnIndex="columnIndex"
-                        :board="board"
-                />
+    <app-drop @drop="moveTaskOrColumn()">
+        <app-drag :transferData="getTransferData(columnIndex)">
+            <div class="column"
+                 @drop="moveTaskOrColumn($event, column.tasks, columnIndex)"
+                 @dragover.prevent
+                 @dragenter.prevent>
+                <div class="flex items-center mb-2 font-bold">
+                    {{ column.name }}
+                </div>
+                <div class="list-reset">
+                    <ColumnTask
+                            v-for="(task, $taskIndex) of column.tasks"
+                            :key="$taskIndex"
+                            :task="task"
+                            :taskIndex="$taskIndex"
+                            :column="column"
+                            :columnIndex="columnIndex"
+                            :board="board"
+                    />
 
-                <input type="text" class="block p-2 w-full bg-transparent" placeholder="+ Enter new task"
-                       @keyup.enter="createTask($event, column.tasks)">
+                    <input type="text" class="block p-2 w-full bg-transparent" placeholder="+ Enter new task"
+                           @keyup.enter="createTask($event, column.tasks)">
+                </div>
             </div>
-        </div>
-    </app-drag>
+        </app-drag>
+    </app-drop>
 </template>
 
 <script>
     import ColumnTask from "./ColumnTask"
     import MovingMixin from "../mixins/MovingMixin"
     import AppDrag from "./AppDrag"
+    import AppDrop from "./AppDrop"
 
     export default {
         name: "BoardColumn",
@@ -38,7 +41,7 @@
           }
         },
         components: {
-            ColumnTask, AppDrag
+            ColumnTask, AppDrag, AppDrop
         },
         mixins: [MovingMixin],
         methods: {
